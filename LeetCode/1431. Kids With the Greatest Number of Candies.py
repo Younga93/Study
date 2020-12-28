8bc8da937bc7bb3cc3b45ab74fcfa8e8d65c9051
Younga Jin <youngajin93@gmail.com>
2020-12-27 23:57:50 -0500

    1431. Kids With the Greatest Number of Candies

---
+++ LeetCode/1431. Kids With the Greatest Number of Candies.py
@@ -0,0 +1,19 @@
+class Solution:
+    def kidsWithCandies(self, candies, extraCandies):
+        Maximum_Candies = max(candies)
+        answers = []
+        for c in candies:
+            if c + extraCandies >= Maximum_Candies:
+                answers.append(True)
+            else:
+                answers.append(False)
+        return answers
+
+
+
+
+solution = Solution()
+
+result = solution.kidsWithCandies([2,3,5,1,3], 3)
+
+print(result)
